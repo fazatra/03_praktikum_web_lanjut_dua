423fd4c497ce0fc93f2b303cc2a10d740132dea9
NareshPratista28 <2141720057@student.polinema.ac.id>
2023-04-03 23:44:54 +0700

    update-Naresh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 @section('isi')
 <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Selamat Datang</h3>
+          <h3 class="card-title">Selamat Datang!</h3>
 
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
